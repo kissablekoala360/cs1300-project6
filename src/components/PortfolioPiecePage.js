7a4cd22313be9bc9eb375e766d0873f75014dff9
john <jfinberg@haverford.edu>
2022-12-09 00:42:16 -0500

    fixed captions

--- src/components/PortfolioPiecePage.js
+++ src/components/PortfolioPiecePage.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Accordian from './Accordian';
 import SlideShow from './SlideShow';
 
-import { RiAlarmWarningFill } from 'react-icons/ri';
-import { HiDocumentMagnifyingGlass } from 'react-icons/hi2';
+import { HiDocumentMagnifyingGlass, HiLink } from 'react-icons/hi2';
 import { FaBrush } from 'react-icons/fa';
 import { BsBookmarkCheckFill } from 'react-icons/bs';
 
@@ -20,9 +19,10 @@ function PortfolioPiecePage(props) {
                 <div className='portfolio-content'>
                     <SlideShow imageList={imageList} />
                     <div className='portfolio-desc'>
-                        <h1>{title}</h1>
-                        <a href={`${url}`} target={"_blank"} style={{marginBottom: 10}}><p>View a live version of the website by clicking here.</p></a>
-                        <Accordian icon={<RiAlarmWarningFill />} title={"Context/Problem"} text={context}></Accordian>
+                        <h1>{title} <a href={`${url}`} target={"_blank"}><HiLink className='project-link'/></a></h1>
+                        <div className='project-description'>
+                            <p className='description'>{context}</p>
+                        </div>
                         <Accordian icon={<HiDocumentMagnifyingGlass />} title={"Research/Feedback"} text={research}></Accordian>
                         <Accordian icon={<FaBrush />} title={"Design Iterations"} text={design}></Accordian>
                         <Accordian icon={<BsBookmarkCheckFill />} title={"Conclusion"} text={conclusion}></Accordian>
